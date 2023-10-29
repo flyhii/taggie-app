161280b4ff0280befd635d6816bd4bda3827a267
Anita <yachi77128230@gmail.com>
2023-10-29 11:21:05 +0800

    datamapper refactor

--- lib/mappers/media_mapper.rb
+++ lib/mappers/media_mapper.rb
@@ -4,47 +4,59 @@ require_relative 'hashtag_mapper'
 
 module FlyHii
   # Provides access to media data
-  class Media
-    attr_reader :media
+  module Instagram
+    class MediaMapper
+      def initialize(token, user_id, gateway_class = Github::Api)
+        @token = ig_token
+        @ig_user_id = user_id
+        @gateway_class = gateway_class
+        @gateway = @gateway_class.new(@token)
+      end
 
-    def initialize(media_data)
-      @media = media_data
-    end
+      def find(hashtag_id)
+        data = @gateway.media(hashtag_id)
+        build_entity(data)
+      end
 
-    def id
-      @media['id']
-    end
+      def build_entity(data)
+        DataMapper.new(data, @token, @gateway_class).build_entity
+      end
 
-    def caption
-      @media['caption']
-    end
+      def id
+        @media['id']
+      end
 
-    def comments_count
-      @media['comments_count']
-    end
+      def caption
+        @media['caption']
+      end
 
-    def like_count
-      @media['like_count']
-    end
+      def comments_count
+        @media['comments_count']
+      end
 
-    def timestamp
-      @media['timestamp']
-    end
+      def like_count
+        @media['like_count']
+      end
 
-    def media_url
-      @media['media_url']
-    end
+      def timestamp
+        @media['timestamp']
+      end
 
-    def children
-      @media['children']
-    end
+      def media_url
+        @media['media_url']
+      end
 
-    def media_type
-      @media['media_type']
-    end
+      def children
+        @media['children']
+      end
 
-    def store_data
-      File.write('spec/fixtures/top_media_results.yml', @media.to_yaml, mode: 'a')
+      def media_type
+        @media['media_type']
+      end
+
+      def store_data
+        File.write('spec/fixtures/top_media_results.yml', @media.to_yaml, mode: 'a')
+      end
     end
   end
 end
