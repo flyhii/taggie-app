6ef9a0dab542b6ce844e3985d9bb3deac01cc799
Anita <yachi77128230@gmail.com>
2023-10-29 11:21:05 +0800

    datamapper refactor

--- lib/gateways/instagram_api.rb
+++ lib/gateways/instagram_api.rb
@@ -7,85 +7,89 @@ require_relative 'hashtag'
 require_relative 'media'
 
 module FlyHii
+  module Instagram
   # Library for Github Web API
-  class InstagramApi
-    API_PROJECT_ROOT = 'https://graph.facebook.com/v18.0'
-    FIELDS = 'id,caption,comments_count,like_count,timestamp, media_url, children, media_type'
-    def initialize(token, user_id)
-      @ig_token = token
-      @ig_user_id = user_id
-    end
-
-    def hashtag(hashtag_name)
-      hashtag_response = Request.new(API_PROJECT_ROOT, @ig_user_id, @ig_token)
-                                .hashtag_url(hashtag_name)
-      hashtag = Hashtag.new(hashtag_response)
-      hashtag.store_data_hashtag
-    end
-
-    def media(hashtag_id)
-      media_response = Request.new(API_PROJECT_ROOT, @ig_user_id, @ig_token)
-                              .media_url(hashtag_id).parsed_response
-      media_response['data'].each do |data|
-        media = Media.new(data)
-        media.store_data
-      end
-    end
-
-    # request url
-    class Request
-      def initialize(resource_root, ig_user_id, token)
-        @resource_root = resource_root
-        @user_id = ig_user_id
-        @token = token
+    class Api
+      # API_PROJECT_ROOT = 'https://graph.facebook.com/v18.0'
+      # FIELDS = 'id,caption,comments_count,like_count,timestamp, media_url, children, media_type'
+      def initialize(token, user_id)
+        @ig_token = token
+        @ig_user_id = user_id
       end
 
-      def hashtag_url(hashtag_name)
-        url = "#{@resource_root}/ig_hashtag_search?user_id=#{@user_id}&q=#{hashtag_name}&access_token=#{@token}"
-        InstagramApiResponseHandler.handle(url)
+      def hashtag(hashtag_name)
+        hashtag_response = Request.new(API_PROJECT_ROOT, @ig_user_id, @ig_token)
+                                  .hashtag_url(hashtag_name)
+        hashtag = Hashtag.new(hashtag_response)
+        hashtag.store_data_hashtag
       end
 
-      def media_url(hashtag_id)
-        url = "#{@resource_root}/#{hashtag_id}/top_media?user_id=#{@user_id}&fields=#{FIELDS}&access_token=#{@token}"
-        InstagramApiResponseHandler.handle(url)
-      end
-    end
-
-    # increase one module to deal with HTTP request
-    module HTTPRequestHandler
-      def self.get(url)
-        HTTParty.get(url)
-      end
-    end
-
-    # take the get url response
-    class InstagramApiResponseHandler
-      def self.handle(url)
-        response = HTTPRequestHandler.get(url) # use new HTTPRequestHandler
-        Response.new(response).tap do |inner_response|
-          raise(inner_response.error) unless inner_response.successful?
+      def media(hashtag_id)
+        media_response = Request.new(API_PROJECT_ROOT, @ig_user_id, @ig_token)
+                                .media_url(hashtag_id).parsed_response
+        media_response['data'].each do |data|
+          media = Media.new(data)
+          media.store_data
         end
       end
-    end
 
-    # Decorates HTTP responses from Instagram with success/error reporting
-    class Response < SimpleDelegator
-      # Represents an unauthorized access error
-      Unauthorized = Class.new(StandardError)
-      # Represents a not found error
-      NotFound = Class.new(StandardError)
+      # request url
+      class Request
+        def initialize(resource_root, ig_user_id, token)
+          @resource_root = resource_root
+          @user_id = ig_user_id
+          @token = token
+        end
 
-      HTTP_ERROR = {
-        401 => Unauthorized,
-        404 => NotFound
-      }.freeze
+        def hashtag_url(hashtag_name)
+          url = "#{@resource_root}/ig_hashtag_search?user_id=#{@user_id}&q=#{hashtag_name}&access_token=#{@token}"
+          InstagramApiResponseHandler.handle(url)
+        end
 
-      def successful?
-        HTTP_ERROR.keys.none?(code)
+        def media_url(hashtag_id)
+          url = "#{@resource_root}/#{hashtag_id}/top_media?user_id=#{@user_id}&fields=#{FIELDS}&access_token=#{@token}"
+          InstagramApiResponseHandler.handle(url)
+        end
       end
 
-      def error
-        HTTP_ERROR[code]
+      # increase one module to deal with HTTP request
+      module HTTPRequestHandler
+        def self.get(url)
+          HTTParty.get(url)
+        end
+      end
+
+      # take the get url response
+      class InstagramApiResponseHandler
+        def self.handle(url)
+          # use new HTTPRequestHandler
+          response = HTTPRequestHandler.get(url)
+
+          Response.new(response).tap do |inner_response|
+            raise(inner_response.error) unless inner_response.successful?
+          end
+        end
+      end
+
+      # Decorates HTTP responses from Instagram with success/error reporting
+      class Response < SimpleDelegator
+        # Represents an unauthorized access error
+        Unauthorized = Class.new(StandardError)
+        # Represents a not found error
+        NotFound = Class.new(StandardError)
+
+        HTTP_ERROR = {
+          401 => Unauthorized,
+          404 => NotFound
+        }.freeze
+
+        def successful?
+          HTTP_ERROR.keys.none?(code)
+        end
+
+        def error
+          HTTP_ERROR[code]
+        end
       end
     end
   end
--- lib/mappers/hashtag_mapper.rb
+++ lib/mappers/hashtag_mapper.rb
@@ -2,20 +2,48 @@
 
 module FlyHii
   # Provides access to hashtag data
-  class Hashtag
-    attr_reader :hashtag
+  module Instagram
+    # Data Mapper: Instagram contributor -> Hashtag entity
+    class HashtagMapper
+      def initialize(hashtag_data, gateway_class = Github::Api)
+        @hashtag = hashtag_data['data'][0]['id']
+        @gateway_class = gateway_class
+        @gateway = @gateway_class.new(@token)
+      end
 
-    def initialize(hashtag_data)
-      @hashtag = hashtag_data['data'][0]['id']
-    end
+      def load_several(url)
+        @gateway.hashtag(url).map do |data|
+          HashtagMapper.build_entity(data)
+        end
+      end
 
-    def hashtag_id
-      @hashtag['hashtag_id']
-    end
+      def self.build_entity(data)
+        DataMapper.new(data).build_entity
+      end
 
-    def store_data_hashtag
-      File.write('spec/fixtures/hashtag_results.yml', @hashtag.to_yaml)
-      @hashtag
+      # Extracts entity specific elements from data structure
+      class DataMapper
+        def initialize(data)
+          @data = data
+        end
+
+        def build_entity
+          Entity::Hashtag.new(
+            id:
+          )
+        end
+
+        private
+
+        def hashtag_id
+          @hashtag['hashtag_id']
+        end
+      end
+
+      def store_data_hashtag
+        File.write('spec/fixtures/hashtag_results.yml', @hashtag.to_yaml)
+        @hashtag
+      end
     end
   end
 end
