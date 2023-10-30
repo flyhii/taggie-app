421ab5777304554e1b27a7bfcc1d17a4b2b88f6e
Anita <yachi77128230@gmail.com>
2023-10-30 16:42:07 +0800

    fix conflict; add config and environment

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'yaml'
+
+module FlyHii
+  # Configuration for the App
+  class App < Roda
+    CONFIG = YAML.safe_load_file('config/secrets.yml')
+    INSTAGRAM_TOKEN = CONFIG['INSTAGRAM_TOKEN']
+  end
+end
