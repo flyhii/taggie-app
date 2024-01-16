5c101e8419bf7892a9ff59c72be5cb3f7e67bd7c
Angel90226 <angel.chung@iss.nthu.edu.tw>
2024-01-16 16:51:50 +0800

    translation available!

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -125,7 +125,6 @@ module FlyHii
             App.configure :production do
               response.expires 60, public: true
             end
-
             view 'media', locals: { post:, rank_list: }
           end
         end
